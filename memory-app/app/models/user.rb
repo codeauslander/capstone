5eb6dbd97a21cf7c548ab7a19698ecd9c9dcba41
codeauslander <codeauslander@gmail.com>
2018-02-25 23:54:04 -0600

    vuejs start

--- memory-app/app/models/user.rb
+++ memory-app/app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_secure_password
   has_many :games
 
-  validates :name, presence: true
+  # validates :name, presence: true
   validates :email, presence: true, uniqueness: true
 
   # def self.from_token_request request
--- memory-app/public/index.js
+++ memory-app/public/index.js
@@ -4,16 +4,117 @@ var HomePage = {
   template: "#home-page",
   data: function() {
     return {
-      message: "Welcome to Vue.js!"
+      cards: "",
+      amountImages: 10,
+      tagName: "ski_jumping",
+      errors: []
     };
   },
-  created: function() {},
+  created: function() {
+    var params = {
+      amount_images: this.amountImages,
+      tag_name: this.tagName
+    };
+    axios
+      .post("/games", params)
+      .then(function(response) {
+        this.cards = response.data;
+      })
+      .catch(
+        function(error) {
+          this.errors = ["No no no."];
+        }.bind(this)
+      );
+  },
   methods: {},
   computed: {}
 };
 
+// Authorization Components
+
+var SignupPage = {
+  template: "#signup-page",
+  data: function() {
+    return {
+      name: "",
+      email: "",
+      password: "",
+      passwordConfirmation: "",
+      errors: []
+    };
+  },
+  methods: {
+    submit: function() {
+      var params = {
+        name: this.name,
+        email: this.email,
+        password: this.password,
+        password_confirmation: this.passwordConfirmation
+      };
+      axios
+        .post("/users", params)
+        .then(function(response) {
+          router.push("/login");
+        })
+        .catch(
+          function(error) {
+            this.errors = error.response.data.errors;
+          }.bind(this)
+        );
+    }
+  }
+};
+
+var LoginPage = {
+  template: "#login-page",
+  data: function() {
+    return {
+      email: "",
+      password: "",
+      errors: []
+    };
+  },
+  methods: {
+    submit: function() {
+      var params = {
+        auth: { email: this.email, password: this.password }
+      };
+      axios
+        .post("/user_token", params)
+        .then(function(response) {
+          axios.defaults.headers.common["Authorization"] =
+            "Bearer " + response.data.jwt;
+          localStorage.setItem("jwt", response.data.jwt);
+          router.push("/");
+        })
+        .catch(
+          function(error) {
+            this.errors = ["Invalid email or password."];
+            this.email = "";
+            this.password = "";
+          }.bind(this)
+        );
+    }
+  }
+};
+
+var LogoutPage = {
+  template: "#home-page",
+  created: function() {
+    axios.defaults.headers.common["Authorization"] = undefined;
+    localStorage.removeItem("jwt");
+    router.push("/");
+  }
+};
+
 var router = new VueRouter({
-  routes: [{ path: "/", component: HomePage }],
+  routes: [
+    { path: "/", component: HomePage },
+    { path: "/games", component: HomePage },
+    { path: "/signup", component: SignupPage },
+    { path: "/login", component: LoginPage },
+    { path: "/logout", component: LogoutPage }
+  ],
   scrollBehavior: function(to, from, savedPosition) {
     return { x: 0, y: 0 };
   }
@@ -21,5 +122,12 @@ var router = new VueRouter({
 
 var app = new Vue({
   el: "#vue-app",
-  router: router
+  router: router,
+  created: function() {
+    var jwt = localStorage.getItem("jwt");
+    console.log(jwt);
+    if (jwt) {
+      axios.defaults.headers.common["Authorization"] = jwt;
+    }
+  }
 });
