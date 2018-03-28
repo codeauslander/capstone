382a78e54bda74b3a38d98e9168ce4b52d5c00f1
codeauslander <codeauslander@gmail.com>
2018-03-28 16:03:25 -0500

    presentation

--- memory-app/config/environments/production.rb
+++ memory-app/config/environments/production.rb
@@ -81,17 +81,21 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.serve_static_assets = true
+  config.assets.digest = true
+  config.assets.compile = true
+
   # config/environments/production.rb
-  config.paperclip_defaults = {
-    storage: :s3,
-    s3_host_name: 's3-us-east-2.amazonaws.com',
-    s3_credentials: {
-      bucket: ENV.fetch('S3_BUCKET_NAME'),
-      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-      s3_region: ENV.fetch('AWS_REGION')
-    }
-  }
+  # config.paperclip_defaults = {
+  #   storage: :s3,
+  #   s3_host_name: 's3-us-east-2.amazonaws.com',
+  #   s3_credentials: {
+  #     bucket: ENV.fetch('S3_BUCKET_NAME'),
+  #     access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+  #     secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+  #     s3_region: ENV.fetch('AWS_REGION')
+  #   }
+  # }
   
 
 end
